7a390de41f3459dc0b5d44bcd102ba1b1124db72
Dustin King <dustinsampers@gmail.com>
2025-03-24 16:54:25 -0400

    Add static site fallback for GitHub Pages

---
+++ public/src/main.js
@@ -0,0 +1,4 @@
+// This is a redirector for GitHub Pages
+// When deployed, GitHub Pages might try to load this file directly.
+// This redirects to the properly built files
+window.location.href = '/theta-tau-voting/'; 
